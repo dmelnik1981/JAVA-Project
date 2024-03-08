5d4ba54768a60ab46fc238dbdfbeaf33db9b43cf
dmelnik1981 <dmelnik1981@mail.ru>
2024-03-08 16:34:05 +0500

    file added test.java

---
+++ Test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Test {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println(st);
+        int number = sc.nextInt();
+        System.out.println(number);
+
+   }
+}
