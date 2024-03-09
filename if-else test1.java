2dbed3c74c26d5a6638c09d139b4d0310af1593c
dmelnik1981 <dmelnik1981@mail.ru>
2024-03-09 17:40:02 +0500

    worked

--- if-else test1.java
+++ if-else test1.java
@@ -1,4 +1,11 @@
-public class if-else test1 {
-    
-
+import java.util.Scanner;
+class MyProg {
+	public static void main(String[] args) {
+		Scanner  sc = new Scanner(System.in);
+        Integer num1 = sc.nextInt();
+        Integer num2 = sc.nextInt();
+        if (num1>num2){
+            System.out.println("Махатма");
+        } else System.out.println("Джавахарлал");
+	}
 }
