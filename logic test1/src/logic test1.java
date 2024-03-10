87ab013bfbf517dd7239a1f3ed085823655976cd
dmelnik1981 <dmelnik1981@mail.ru>
2024-03-10 11:44:02 +0500

    Worked ... OK

---
+++ logic test1/src/logic test1.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        Integer age = 23;
+        Integer tasks = 4000;
+        if (age > 21 && tasks > 500) {
+            System.out.println("Добро пожаловать!");
+        }
+    }
+}
